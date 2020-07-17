19c8853f0f8ba52aca458f6f5eedef4e2c5beddb
haoweitech <git@haowei.tech>
2020-07-17 11:56:44 +0800

    初始化项目

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
   <div>
     <m-row v-for="(v,k) in [
-      {size:'small',text:'小尺寸'} ,
+      {size:'large',text:'超大尺寸'},
       {size:'default',text:'默认尺寸'}, 
-      {size:'large',text:'超大尺寸'}
+      {size:'small',text:'小尺寸'} ,
     ]"
     :key="k"
     :style="{marginBottom: '15px'}"><m-input :size="v.size" :placeholder="v.text" :style="{maxWidth: '200px'}"/></m-row>
